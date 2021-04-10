2f66e34d2e7362663e3785557483fe2df1fa0de9
Mauri <Mauri@192.168.0.14>
2021-04-10 20:52:09 -0300

    Agregado de hashcode y tostring Rios Mauricio

--- src/main/java/ar/edu/unju/edm/model/Calculadora.java
+++ src/main/java/ar/edu/unju/edm/model/Calculadora.java
@@ -47,6 +47,40 @@ public class Calculadora {
 	public void setB(int b) {
 		this.b = b;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + a;
+		result = prime * result + b;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Calculadora other = (Calculadora) obj;
+		if (a != other.a)
+			return false;
+		if (b != other.b)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculadora [a=" + a + ", b=" + b + ", sumar()=" + sumar() + ", restar()=" + restar()
+				+ ", multiplicar()=" + multiplicar() + ", dividir()=" + dividir() + ", getA()=" + getA() + ", getB()="
+				+ getB() + ", hashCode()=" + hashCode() + ", getClass()=" + getClass() + ", toString()="
+				+ super.toString() + "]";
+	}
+	
 	
 	
 }
--- src/main/java/ar/edu/unju/edm/model/Provincia.java
+++ src/main/java/ar/edu/unju/edm/model/Provincia.java
@@ -35,6 +35,40 @@ public class Provincia {
 		this.nombre = nombre;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Provincia other = (Provincia) obj;
+		if (codigo != other.codigo)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Provincia [codigo=" + codigo + ", nombre=" + nombre + ", getCodigo()=" + getCodigo() + ", getNombre()="
+				+ getNombre() + ", hashCode()=" + hashCode() + ", getClass()=" + getClass() + ", toString()="
+				+ super.toString() + "]";
+	}
 	
 }
 
